b3a0cfd53486ec238a672b46ae3521ca19919492
milcaponce <milcaponce.dev@gmail.com>
2025-08-31 20:23:33 +0200

    feat: initial commit of tech-support-api project

---
+++ src/main/java/dev/milca/api/tech_support_api/TechSupportApiApplication.java
@@ -0,0 +1,13 @@
+package dev.milca.api.tech_support_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSupportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSupportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/milca/api/tech_support_api/TechSupportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.milca.api.tech_support_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSupportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
